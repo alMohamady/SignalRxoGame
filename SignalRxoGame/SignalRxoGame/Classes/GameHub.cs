fa46bc76cfc804a2b278e7c6b28130ec05d89439
mohamady-div <major_2047@hotmail.com>
2025-04-11 01:37:37 +0200

    Add project files.

---
+++ SignalRxoGame/SignalRxoGame/Classes/GameHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRxoGame.Classes
+{
+    public class GameHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Connector Id '{Context.ConnectionId}' connected");
+            return base.OnConnectedAsync();
+        }
+    }
+}
